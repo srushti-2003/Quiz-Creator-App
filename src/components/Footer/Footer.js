23383803904dddf26f7758ff0535d4e0f420f0ec
srushti-2003 <srushti.rce20@sot.pdpu.ac.in>
2023-08-06 17:32:23 +0530

    first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+const Footer = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginBottom: 10,
+      }}
+    >
+      Made with ♥ by {"Srushti"}
+    
+    </div>
+  );
+};
+
+export default Footer;
